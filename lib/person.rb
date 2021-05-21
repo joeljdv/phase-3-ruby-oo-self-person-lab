71fd550abc630cb43f71da60af1d605362473be6
joeljdv <joeldiazjdv@gmail.com>
2021-05-21 09:38:16 -0400

    commit tree 62fd73dcc6bcf141f94e040baa9d496b3595fee6

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,33 @@
 # your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, num = 25, index = 8, points = 8)
+        @name = name
+        @bank_account = num
+        @happiness = index
+        @hygiene = points
+    end
+
+    def happiness=(index = 8)
+            if index < 0
+                index = 0
+            elsif index > 10
+                index = 10
+            end
+            @happiness = index
+    end
+    
+    def hygiene=(points = 8)
+        if points > 10
+            points = 10
+        elsif points < 0
+            points = 0
+        end
+        @hygiene = points
+    end
+
+   
+end
